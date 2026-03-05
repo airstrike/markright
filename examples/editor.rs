a6188687f76720a512a62c6b7bc78ca4d5d2b23b
Andy Terra <152812+airstrike@users.noreply.github.com>
2026-03-05 12:01:31 -0500

    feat(example): add IBM Plex font loading and polish editor example
    Load IBM Plex Sans (Regular, Bold, Italic, Bold Italic) and IBM Plex
    Mono (Regular) fonts from GitHub URLs at startup using reqwest. Set the
    default font to IBM Plex Sans, pass IBM Plex Mono as monospace font to
    the editor widget, and wire up all cursor movement actions.

--- examples/editor.rs
+++ examples/editor.rs
@@ -1,11 +1,23 @@
 use iced::widget::{column, container, text};
-use iced::{Color, Element, Fill, Length, Padding};
+use iced::{Color, Element, Fill, Font, Length, Padding, Task};
 
 use markright::Document;
+
+/// IBM Plex Sans Regular
+const PLEX_SANS_REGULAR: &str = "https://raw.githubusercontent.com/IBM/plex/master/packages/plex-sans/fonts/complete/ttf/IBMPlexSans-Regular.ttf";
+/// IBM Plex Sans Bold
+const PLEX_SANS_BOLD: &str = "https://raw.githubusercontent.com/IBM/plex/master/packages/plex-sans/fonts/complete/ttf/IBMPlexSans-Bold.ttf";
+/// IBM Plex Sans Italic
+const PLEX_SANS_ITALIC: &str = "https://raw.githubusercontent.com/IBM/plex/master/packages/plex-sans/fonts/complete/ttf/IBMPlexSans-Italic.ttf";
+/// IBM Plex Sans Bold Italic
+const PLEX_SANS_BOLD_ITALIC: &str = "https://raw.githubusercontent.com/IBM/plex/master/packages/plex-sans/fonts/complete/ttf/IBMPlexSans-BoldItalic.ttf";
+/// IBM Plex Mono Regular
+const PLEX_MONO_REGULAR: &str = "https://raw.githubusercontent.com/IBM/plex/master/packages/plex-mono/fonts/complete/ttf/IBMPlexMono-Regular.ttf";
 
 fn main() -> iced::Result {
     iced::application(App::new, App::update, App::view)
         .title("Markright")
+        .default_font(Font::with_name("IBM Plex Sans"))
         .run()
 }
 
@@ -16,10 +28,11 @@ struct App {
 #[derive(Debug, Clone)]
 enum Message {
     Edit(markright::Action),
+    FontLoaded(Result<(), iced::font::Error>),
 }
 
 impl App {
-    fn new() -> (Self, iced::Task<Message>) {
+    fn new() -> (Self, Task<Message>) {
         let sample = "\
 # Welcome to Markright
 
@@ -55,11 +68,27 @@ Try editing this text! You can use **bold**, *italic*, ***bold italic***, and `i
 ##### Heading 5
 ###### Heading 6";
 
+        let font_tasks = Task::batch(
+            [
+                PLEX_SANS_REGULAR,
+                PLEX_SANS_BOLD,
+                PLEX_SANS_ITALIC,
+                PLEX_SANS_BOLD_ITALIC,
+                PLEX_MONO_REGULAR,
+            ]
+            .into_iter()
+            .map(|url| {
+                Task::future(fetch_font(url.to_owned()))
+                    .then(iced::font::load)
+                    .map(Message::FontLoaded)
+            }),
+        );
+
         (
             Self {
                 document: Document::from_markdown(sample),
             },
-            iced::Task::none(),
+            font_tasks,
         )
     }
 
@@ -70,15 +99,24 @@ Try editing this text! You can use **bold**, *italic*, ***bold italic***, and `i
                 markright::Action::Delete => self.document.delete(),
                 markright::Action::Backspace => self.document.backspace(),
                 markright::Action::Enter => self.document.enter(),
-                markright::Action::Move(_motion) => {
-                    // TODO: implement cursor movement in document model
-                }
+                markright::Action::Move(motion) => match motion {
+                    markright::Motion::Left => self.document.move_left(),
+                    markright::Motion::Right => self.document.move_right(),
+                    markright::Motion::Up => self.document.move_up(),
+                    markright::Motion::Down => self.document.move_down(),
+                    markright::Motion::Home => self.document.move_home(),
+                    markright::Motion::End => self.document.move_end(),
+                    _ => {}
+                },
                 markright::Action::Click { block, offset } => {
                     self.document.set_active_block(*block);
                     self.document.set_cursor(*offset);
                 }
                 _ => {}
             },
+            Message::FontLoaded(_) => {
+                // Fonts are loaded into the global font system automatically.
+            }
         }
     }
 
@@ -95,7 +133,8 @@ Try editing this text! You can use **bold**, *italic*, ***bold italic***, and `i
         let editor = markright::editor(&self.document)
             .on_action(Message::Edit)
             .padding(20)
-            .size(16);
+            .size(16)
+            .monospace_font(Font::with_name("IBM Plex Mono"));
 
         let content = column![
             editor,
@@ -112,4 +151,15 @@ Try editing this text! You can use **bold**, *italic*, ***bold italic***, and `i
             .height(Fill)
             .into()
     }
+}
+
+/// Fetch font bytes from a URL.
+async fn fetch_font(url: String) -> Vec<u8> {
+    reqwest::get(&url)
+        .await
+        .expect("font fetch failed")
+        .bytes()
+        .await
+        .expect("font bytes failed")
+        .to_vec()
 }
